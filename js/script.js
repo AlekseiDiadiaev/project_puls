d4fe3952433dc33351890edab4333d9a088ac329
Aledya <example@gmail.com>
2022-06-10 09:31:31 +0300

    add carousel

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 700,
+        /* adaptiveHeight: true, */
+        prevArrow: '<button type="button" class="slick-prev"><img src="icon/slide_left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icon/slide_right.png"></button>',
+        responsive: [
+            {
+              breakpoint: 991,
+              settings: {
+                dots: true,
+                arrows: false
+              }
+            }  
+          ]
+      });
+  });
