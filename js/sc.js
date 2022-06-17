bb1729851c957c5e055aad63567e5aecf596acf4
Aledya <example@gmail.com>
2022-06-17 09:52:08 +0300

    Toggler card

--- js/sc.js
+++ js/sc.js
@@ -1,3 +1,4 @@
+// Реализация табов
 let elTab = document.getElementsByClassName("catalog__tab");
 
 function tab1() {
@@ -20,17 +21,30 @@ elTab[0].onclick = tab1;
 elTab[1].onclick = tab2;
 elTab[2].onclick = tab3;
 
-elMain = document.getElementsByClassName("catalog-item__main");
-elSecond = document.getElementsByClassName("catalog-item__second");
+//Переключение вида карточки
+let elMain = document.querySelectorAll(".catalog-item__link-main");
+let elSecond = document.querySelectorAll(".catalog-item__link-second");
 
-function mainx(){
-    elSecond[0].className = "catalog-item__second";
-    elMain[0].className = "catalog-item__main";
+function main(i){
+    elSecond[i].className = "catalog-item__second";
+    elMain[i].className = "catalog-item__main";
 }
-function secondx(){
+function second(){
     elSecond[0].className = "catalog-item__second catalog-item__second_active";
     elMain[0].className = "catalog-item__main catalog-item__main_active";
 }
+elMain.forEach((item, i ) => {
+    item.addEventListener('click', () => {
+       let mainDivCard = item.parentElement;
+       mainDivCard.className = "catalog-item__main catalog-item__main_active";
+       mainDivCard.nextSibling.nextSibling.className = "catalog-item__second catalog-item__second_active"; 
+    });
+});
 
-document.getElementById('xxx').onclick = mainx;
-document.getElementById('yyy').onclick = secondx;
+elSecond.forEach((item, i ) => {
+    item.addEventListener('click', () => {
+        let secondDivCard = item.parentElement;
+        secondDivCard.className = "catalog-item__second";
+        secondDivCard.previousSibling.previousSibling.className = "catalog-item__main"; 
+     });
+ });
