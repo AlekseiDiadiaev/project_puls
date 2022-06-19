9a386db5c7edfd46f26bc8e6e7397b05e305c06c
Aledya <example@gmail.com>
2022-06-19 07:15:10 +0300

    Add script modal

--- js/tab.js
+++ js/tab.js
@@ -44,3 +44,50 @@ elSecond.forEach((item, i ) => {
         secondDivCard.previousSibling.previousSibling.className = "catalog-item__main"; 
      });
  });
+
+// Модальные окна
+ //Вызов окна консультаций
+ document.querySelector('.button_header').onclick = getModalConsult;  
+ document.querySelector('.button_main').onclick = getModalConsult; 
+
+function getModalConsult () {
+    document.querySelector('.overlay').className ="overlay overlay_active";
+    document.querySelector('.modal_consult-hidden').className = "modal";
+}
+//Закрытие
+document.querySelectorAll(".modal__close").forEach(item => {    
+    item.addEventListener("click", () => {
+        document.querySelector('.overlay').className ="overlay";
+        document.querySelectorAll('.modal').forEach((item,i) =>{
+            switch (i){
+                case 0: 
+                item.className = "modal modal_consult-hidden";
+                break; 
+                case 1: 
+                item.className = "modal modal_buy-hidden";
+                break; 
+                case 2: 
+                item.className = "modal modal_mini-hidden";
+                break; 
+            } 
+        });
+    }); 
+});
+//Купить
+document.querySelectorAll(".button_catalog-item").forEach(item => {
+    item.addEventListener('click', () => {
+        const nameProduct  = item.parentNode.parentNode.querySelector('.catalog-item__subtitle').textContent;
+        document.getElementById('modal__buy').querySelector('.modal__descr').textContent = nameProduct;
+        document.querySelector('.overlay').className ="overlay overlay_active";
+        document.querySelector('.modal_buy-hidden').className = "modal";
+    });
+}); 
+//Вызов окна спасибо
+document.querySelectorAll('.consultation__form').forEach(item =>{
+    item.addEventListener('submit', () => {
+        document.querySelector('.overlay').className ="overlay overlay_active";
+        document.querySelector('.modal_consult-hidden').className = "modal modal_buy-hidden";
+        document.querySelector('.modal_buy-hidden').className = "modal modal_buy-hidden";
+        document.querySelector('.modal_mini-hidden').className = "modal";
+    });
+});
